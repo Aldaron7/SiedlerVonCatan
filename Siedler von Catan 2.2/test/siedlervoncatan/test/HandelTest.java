bb802090f8f6eb518a77afe0ac297d12535e8235
Julian Riegraf <Julian Riegraf@10.1.117.137>
2016-07-01 11:47:51 +0200

    add HandelTest

---
+++ Siedler von Catan 2.2/test/siedlervoncatan/test/HandelTest.java
@@ -0,0 +1,50 @@
+package siedlervoncatan.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import siedlervoncatan.enums.Farbe;
+import siedlervoncatan.enums.Rohstoff;
+import siedlervoncatan.spiel.Spiel;
+import siedlervoncatan.spiel.Spieler;
+import siedlervoncatan.utility.Handel;
+
+public class HandelTest {
+
+	@Test
+	public void test() {
+		
+		Spiel spiel = new Spiel();
+		
+		ObservableList<Rohstoff> angebot = FXCollections.observableArrayList(Rohstoff.ERZ, Rohstoff.ERZ, Rohstoff.KORN, Rohstoff.KORN, Rohstoff.KORN);
+		ObservableList<Rohstoff> nachfrage = FXCollections.observableArrayList(Rohstoff.HOLZ, Rohstoff.HOLZ,Rohstoff.HOLZ,Rohstoff.HOLZ);
+
+		
+		
+		Spieler spieler1 = new Spieler("Marcel", Farbe.BRAUN, spiel);
+		spieler1.addKarten(angebot);
+		
+		Spieler spieler2 = new Spieler("Artem", Farbe.BLAU, spiel);
+		spieler2.addKarten(nachfrage);
+		
+		Handel handel = new Handel();
+		
+		handel.setAnbieter(spieler1);
+		handel.setNachfrager(spieler2);
+		
+		
+		// Handel bestätigen bzw. ablehnen
+		
+		handel.handeln();
+		
+		
+		
+		
+		
+		
+	}
+
+}
